988767b64b9641d9d96a02e31635dcc56d615084
PRESSO <caff1nepill@gmail.com>
2023-09-09 23:51:06 +0900

    Feat: boilerplate settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		fontFamily: {
+			sans: [
+				'Pretendard Variable',
+				'Pretendard',
+				'-apple-system',
+				'BlinkMacSystemFont',
+				'system-ui',
+				'Roboto',
+				'Helvetica Neue',
+				'Segoe UI',
+				'Apple SD Gothic Neo',
+				'Noto Sans KR',
+				'Malgun Gothic',
+				'Apple Color Emoji',
+				'Segoe UI Emoji',
+				'Segoe UI Symbol',
+				'sans-serif',
+			],
+		},
+	},
+	plugins: [],
+};
+export default config;
